c4859c246307ed10d66bdcf45ebee6516abb0072
Tcheutchoua Steve <tcheutchouasteve@gmail.com>
2020-10-14 07:42:20 +0100

    adds union operation to Int sets

--- src/main/scala/example/insects.scala
+++ src/main/scala/example/insects.scala
@@ -8,12 +8,13 @@ object insects {
 abstract class IntSet{
   def incl(x: Int): IntSet
   def contains(x: Int): Boolean
+  def union(other: IntSet): IntSet
 }
 
 class Empty extends IntSet{
    def contains(x: Int): Boolean = false
    def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty)
-
+   def union(other: IntSet) = other
   override def toString: String = "."
 }
 
@@ -26,7 +27,9 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet{
    def incl(x: Int): IntSet =
     if (x < elem) new NonEmpty(elem, left incl x, right)
     else if (x > elem) new NonEmpty(elem, left, right incl x)
-     else this
+    else this
 
+    def union (other: IntSet): IntSet =
+      ((left union right) union other) incl elem
   override def toString: String = "{" + left + elem + right + "}"
 }
